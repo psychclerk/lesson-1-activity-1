bc6d0887fc37530d9ef975d6048c2a2a0e0446f6
psychclerk <78206666+psychclerk@users.noreply.github.com>
2025-04-05 22:04:53 +0530

    welcome

---
+++ variable.py
@@ -0,0 +1,12 @@
+A=5
+n=66
+i=59
+k=89
+a=49
+print(A)
+print(n)
+print(i)
+print(k)
+print(a)
+print(a+n)
+print(i+k)
---
+++ welcome.py
@@ -0,0 +1,4 @@
+print("my name is anika.I am 10 years old.")
+print("how are you?")
+print(500)
+print("how are you",500)
